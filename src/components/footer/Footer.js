624a9ab979536a791865ad141c98bbf3b4537483
manojat012 <manoja.t012@gmail.com>
2024-07-19 20:24:22 +0530

    Contact Page and Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,48 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faInstagram, faYoutube, faWhatsapp, faTelegram } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUp, faMapMarked } from "@fortawesome/free-solid-svg-icons";
 import "./Footer.css";
 
-const Footer = (props) => {
+
+
+const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-links">
-        <Link to="/about">About</Link>
-        <span className="footer-link-slash">/</span>
-        <Link to="/Sermons">Teachings</Link>
-        <span className="footer-link-slash">/</span>
-        <Link to="/contact">Contact</Link>
-        <span className="footer-link-slash">/</span>
-        <Link to="/live">Watch Live</Link>
-      </div>
-      <div className="row">
-        <div className="col-sm">
-          <p>
-            SVAS
-            <br />
-            Hyderabad
-            <br />
-            2023 &copy; SVAS
-          </p>
+      <div className="footer-container">
+        <div className="footer-left">
+          <img
+            src="/img/SvasLogoFinal-v2.jpeg"
+            alt="Logo"
+            className="footer-logo"
+            style={{ width: "auto", height: "55px" }}
+          />
         </div>
-        <div className="col-sm">
-          <p className="footer-social-title">STAY CONNECTED</p>
+        <div className="footer-center">
           <div className="footer-social-links">
-            <a href="https://www.facebook.com/">
-              <i className="fab fa-facebook-f"></i>
+            <a href="https://www.youtube.com" aria-label="Youtube">
+              <FontAwesomeIcon icon={faYoutube} />
             </a>
-            <a href="https://twitter.com/">
-              <i className="fab fa-twitter"></i>
+            <a href="https://www.behance.net" aria-label="Whatsapp">
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </a>
+            <a href="https://www.dribbble.com" aria-label="Dribbble">
+              <FontAwesomeIcon icon={faTelegram} />
+            </a>
+            <a href="https://www.instagram.com" aria-label="Instagram">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a href="https://www.google.com/maps" aria-label="Maps">
+              <FontAwesomeIcon icon={faMapMarked} />
             </a>
           </div>
         </div>
-        <div className="col-sm d-none d-sm-none d-md-none d-lg-block">
-          <img
-            alt="footer-logo"
-            // srcSet="img/logo_footer@2x.png 2x"
-            className="footer-logo float-right"
-            src="/img/SvasLogoFinal-v2.jpeg"
-          />
+        <div className="footer-right">
+          <a href="/terms-and-conditions" className="footer-terms">
+            RULES AND REGULATIONS
+          </a>
         </div>
+      </div>
+      <div className="footer-scroll-up">
+        <a href="#top" aria-label="Scroll to top">
+          <FontAwesomeIcon icon={faArrowUp} />
+        </a>
       </div>
     </div>
   );
