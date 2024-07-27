3ed504d013646541e72c39961635db24e048c1f8
Manideep1999 <manideep2705@gmail.com>
2024-07-27 04:15:28 -0500

    Mani - First live server deployment using gh-pages

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import WordForTheWeekPage from "./components/word-for-the-week/WordForTheWeekPag
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <SBC>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
