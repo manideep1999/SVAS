710aef86d844ce0b6e0dfb7df94973535a55ea46
Kalyani Veena <kalyani.baladaru@mxplayer.in>
2024-09-10 21:04:43 +0530

    Meditation tab

--- src/components/meditation/Meditation-Content.js
+++ src/components/meditation/Meditation-Content.js
@@ -1,5 +1,59 @@
 import React from "react";
+import "./Meditation-Content.css";
 function MeditationContent() {
-  return <section></section>;
+  return (
+    <>
+      <section className="container">
+        <div className="row">
+          <div className="col-md-6 ">
+            <img
+              src="img/meditation/painting.jpg"
+              className="img-fit"
+              alt="masked painting"
+            ></img>
+          </div>
+          <div className="col-md-6 ">
+            <div className="space-item">
+              <h2 className="heading-text">MEDITATION...</h2>
+            </div>
+            <div className="space-item">
+              <p className="data-text small-header">
+                is not for relaxation. What this Magnificient tool does is much
+                beyond our usually limited thinking.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h2 className="heading-text move-down">What does Meditation do?</h2>
+          </div>
+          <div className="col-md-6">
+            <div className="space-item">
+              <h3 className="sub-header">An Excerpt from the discourse</h3>
+              <p className="data-text space-text">
+                “Meditation gives you peace. It gives you tranquility, gives you
+                energy, gives you happiness, enhances your thinking, gives
+                clarity to your thoughts. Brings integrity into your life. That
+                integrity will become an indication of your success. If you
+                understand that integrity, it will help to know which way is
+                correct for you. It also helps to choose the way you need to
+                walk,it helps to filter your thoughts and choose the proper ones
+                which will lead you to the intended goal and become successful.
+                You ought to understand the fact that -It is mediation that
+                contributes to your success bringing out the energy that is
+                inherent and hidden within you, giving back your true power that
+                you have ignored and left helplessly within you and it drives
+                you forward every moment. This is what meditation is...."
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 export default MeditationContent;
--- src/components/meditation/Meditation.js
+++ src/components/meditation/Meditation.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Meditation.css";
 import MeditationHeader from "./MeditationHeader";
+import MeditationContent from "./Meditation-Content";
 
 function Meditation() {
   return (
     <>
       <MeditationHeader></MeditationHeader>
+      <MeditationContent></MeditationContent>
     </>
   );
 }
